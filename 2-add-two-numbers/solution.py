f4c19d67f2367d4559a43d5ab1533773f330d3e0
Pushkar Patil <pushu200020@gmail.com>
2025-12-25 12:56:29 +0530

    Auto-sync: Add Two Numbers | leetcode=8 ms/17.4 MB

---
+++ 2-add-two-numbers/solution.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        curr1 = l1
+        curr2 = l2
+        dummy = ListNode(0)
+        curr3 = dummy
+        carry = 0
+        while curr1 or curr2 or carry:
+            v1 = curr1.val if curr1 else 0
+            v2 = curr2.val if curr2 else 0
+            s = v1 + v2 + carry
+            carry = s // 10
+            n = s % 10
+            curr3.next = ListNode(n)
+            curr3 = curr3.next
+            # print('l3 ',curr3.val)
+            curr1 = curr1.next if curr1 else None
+            curr2 = curr2.next if curr2 else None
+        return dummy.next
