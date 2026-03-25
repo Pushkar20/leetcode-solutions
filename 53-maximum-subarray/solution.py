fff9637ae3990afc5f241e23a5f019838be21485
Pushkar Patil <pushu200020@gmail.com>
2026-03-25 19:05:01 +0530

    Auto-sync: Maximum Subarray | leetcode=N/A/N/A

---
+++ 53-maximum-subarray/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sum_ = 0
+        max_ = 0
+        for i in nums:
+            sum_ = sum_ + i
+            if sum_ > 0 and sum_ > max_:
+                max_ = sum_
+            elif sum_ < 0:
+                sum_ = 0
+        return max_
