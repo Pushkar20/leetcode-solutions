94ee84a99cdbba48414c350a38a5c0315cb89845
Pushkar Patil <pushu200020@gmail.com>
2026-04-16 19:00:35 +0530

    Auto-sync: Search Insert Position | leetcode=N/A/N/A

---
+++ 35-search-insert-position/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums) - 1
+        i = 0
+        while i <= n:
+            mid = (i + n) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                i = mid + 1
+            else:
+                n = mid - 1
+        return mid+1
