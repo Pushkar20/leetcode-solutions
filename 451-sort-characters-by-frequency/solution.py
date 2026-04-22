444c3f017685522a40589abaee24e15f0d7611cb
Pushkar Patil <pushu200020@gmail.com>
2026-04-22 10:49:37 +0530

    Auto-sync: Sort Characters By Frequency | leetcode=7 ms/20.1 MB

---
+++ 451-sort-characters-by-frequency/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        from collections import Counter
+        freq = Counter(s)
+        sorted_chars = sorted(freq.keys(), key=lambda x: freq[x], reverse=True)
+        result = ""
+        for ch in sorted_chars:
+            result += ch * freq[ch]
+        return result
